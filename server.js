84cc181a8073d7230c39278bbebe9caa565e726c
akabab <ycribier@student.42.fr>
2015-05-23 17:08:38 +0200

    init

---
+++ server.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var debug = require('debug')('wunderfun:server');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+var port = process.env.PORT || '8000';
+app.set('port', port);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+var onError = function(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+var onListening = function() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+};
+
+
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+
+    socket.on('set pseudo', function(pseudo) {
+        socket.pseudo = pseudo;
+    });
+
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        //send to everyone exept me
+        socket.broadcast.emit('chat message', msg);
+
+        //send to all
+        io.emit('chat message', "all" + msg);
+    });
+});
+
+//global io
+require("./lib/Socket")(io);
+
+var Relayr = require("./lib/Relayr");
+Relayr.setIO(io);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+module.exports = app;
